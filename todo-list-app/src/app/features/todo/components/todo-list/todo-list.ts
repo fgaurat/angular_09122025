c1fc2a22e857ce263fd09a18b31b592eff000b83
Fred <fgaurat@gmail.com>
2025-12-10 16:56:43 +0100

    add mattable

--- todo-list-app/src/app/features/todo/components/todo-list/todo-list.ts
+++ todo-list-app/src/app/features/todo/components/todo-list/todo-list.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { TodoService } from '../../services/todo.service';
+import { AsyncPipe } from '@angular/common';
+import { Todo, Todos } from '../../entities/todo';
+import { map } from 'rxjs';
+import { MatTableModule } from '@angular/material/table';
 
 @Component({
   selector: 'app-todo-list',
-  imports: [],
+  imports: [AsyncPipe,MatTableModule],
   templateUrl: './todo-list.html',
   styleUrl: './todo-list.css',
 })
-export class TodoList {
+export class TodoList{
+
+  todoService:TodoService = inject(TodoService)
+  todos$ = this.todoService.findAll().pipe(
+    map((t:Todos) => t.map((todo:Todo) => ({...todo,title:todo.title.toUpperCase()}) )
+  ))
+  displayedColumns:string[] = ['id']
 
 }
--- todo-list-app/src/app/features/todo/services/todo.service.ts
+++ todo-list-app/src/app/features/todo/services/todo.service.ts
@@ -1,13 +1,18 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Todos } from '../entities/todo';
+import { Observable } from 'rxjs';
+import { environment } from '../../../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TodoService {
+  
+  private http:HttpClient = inject(HttpClient)
 
-
-  findAll(){
-    
+  findAll():Observable<Todos>{
+    return this.http.get<Todos>(environment.url_todos);
   }
 
 }
