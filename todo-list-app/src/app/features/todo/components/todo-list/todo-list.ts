bc28d70637e92c36c5f3397aa0b4f9d627d20042
Fred <fgaurat@gmail.com>
2025-12-11 09:46:07 +0100

    add checkbox

--- todo-list-app/src/app/features/todo/components/todo-list/todo-list.ts
+++ todo-list-app/src/app/features/todo/components/todo-list/todo-list.ts
@@ -4,19 +4,21 @@ import { AsyncPipe } from '@angular/common';
 import { Todo, Todos } from '../../entities/todo';
 import { map } from 'rxjs';
 import { MatTableModule } from '@angular/material/table';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 @Component({
   selector: 'app-todo-list',
-  imports: [AsyncPipe,MatTableModule],
+  imports: [AsyncPipe,MatTableModule,MatCheckboxModule],
   templateUrl: './todo-list.html',
   styleUrl: './todo-list.css',
 })
 export class TodoList{
 
   todoService:TodoService = inject(TodoService)
-  todos$ = this.todoService.findAll().pipe(
-    map((t:Todos) => t.map((todo:Todo) => ({...todo,title:todo.title.toUpperCase()}) )
-  ))
-  displayedColumns:string[] = ['id']
+  // todos$ = this.todoService.findAll().pipe(
+  //   map((t:Todos) => t.map((todo:Todo) => ({...todo,title:todo.title.toUpperCase()}) )
+  // ))
+  todos$ = this.todoService.findAll()
+  displayedColumns:string[] = ['id','userId','title','completed','chkCompleted']
 
 }
